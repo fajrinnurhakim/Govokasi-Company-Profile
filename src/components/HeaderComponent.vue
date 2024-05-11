e246d59d5c9e73ca67798ea18bea3291830fea84
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-05-11 22:04:07 +0700

    change: header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,7 +2,7 @@
   <div class="navigation">
     <img src="/logo.png" alt="logo" class="logo" /><img />
     <ul class="menu" :class="{ aktif: isMenuActive }">
-      <li><a href="#home">Home</a></li>
+      <li><a href="#home" :class="{ aktif: isMenuActive }">Home</a></li>
       <li><a href="#service">Service</a></li>
       <li><a href="#company">Company</a></li>
       <li><a href="#about">About us</a></li>
@@ -49,12 +49,11 @@ export default {
   margin: 0;
   display: flex;
   list-style: none;
-  padding: 0;
+  font-size: 12pt;
 }
 
 .menu li {
   padding-left: 0;
-  margin-right: 10px;
 }
 
 .menu li a {
@@ -62,10 +61,13 @@ export default {
   color: black;
   text-decoration: none;
   display: inline-block;
+  padding: 12px;
 }
 
 .menu li a:hover {
-  color: blue;
+  color: #110f5e;
+  background-color: #fddc58;
+  border-radius: 12px;
 }
 
 @media screen and (max-width: 768px) {
@@ -74,18 +76,20 @@ export default {
   }
 
   .menu.aktif {
-    display: inline-block;
+    display: block;
     position: absolute;
     top: 50px;
     background: rgba(247, 247, 247, 1);
-    padding: 10px 20px;
     right: 0;
     left: 0;
+    text-align: center;
   }
 
   .menu.aktif li a {
-    padding: 10px;
-    display: inline-block !important;
+    display: block;
+  }
+  .menu.aktif li a {
+    border-radius: 0px;
   }
 
   .button {
